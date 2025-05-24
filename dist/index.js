bc22058ff21fb75b37c6ba5deee1a3f7e1c4a1ab
Arjun Attam <arjunattam@gmail.com>
2025-05-24 16:38:37 +0530

    errthing

--- dist/index.js
+++ dist/index.js
@@ -29389,6 +29389,14 @@ async function run() {
         if (authKey) {
             headers['Authorization'] = `Bearer ${authKey}`;
         }
+        const runtimeConfig = core.getMultilineInput('runtime-config');
+        if (runtimeConfig) {
+            const config = runtimeConfig.map((line) => {
+                const [key, value] = line.split(':');
+                return { [key.trim()]: value.trim() };
+            });
+            console.log(`Runtime config: ${JSON.stringify(config)}`);
+        }
         const branch = await getBranchName();
         console.log(`Branch name: ${branch}`);
         const response = await fetch("https://dispatch.empirical.run/v1/trigger", {
